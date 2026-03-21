66eb54f8f1feefd1aa1de432fba380d402c24fd9
Kyon <139816800+JoaoAntP@users.noreply.github.com>
2026-03-20 23:02:53 -0300

    .

---
+++ Curso_de_Python_LuizOtávio/Modulo-3/fateamentoStrings.py
@@ -0,0 +1,7 @@
+variavel =  'olá mundo'
+print(variavel[4::2])
+#[i:f:p]
+#len server para contar caracteres
+print(len(variavel[3]))# neste caso ele pega quantos caracteres tem nesse 'local'
+print(len(variavel))#contando tamanho total de string
+print(variavel[::-1])#funciona de do inicio até o fim so que de trás para frente e invente a palava -987654321
